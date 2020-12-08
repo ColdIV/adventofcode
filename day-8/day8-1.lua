563d4a2bab4197e34c203695bb0050d517ab39e4
ColdIV <yufoolme@gmail.com>
2020-12-08 16:17:14 +0100

    add solution for day 8

---
+++ day-8/day8-1.lua
@@ -0,0 +1,53 @@
+function file_exists(file)
+  local f = io.open(file, "rb")
+  if f then f:close() end
+  return f ~= nil
+end
+
+function lines_from(file)
+  if not file_exists(file) then return {} end
+  lines = {}
+  for line in io.lines(file) do 
+    lines[#lines + 1] = line
+  end
+  return lines
+end
+
+---
+
+local file = 'data.txt'
+local lines = lines_from(file)
+local commands = {}
+local acc = 0
+
+for k, v in pairs(lines) do
+	local command = string.sub(v, 1, 3)
+	local value = tonumber(string.sub(v, 5))
+	commands[k] = {command, value}
+	commands[k].command = command
+	commands[k].value = value
+	-- print (commands[k].command, commands[k].value)
+end
+
+local position = 1
+local commandsUsed = {}
+
+function run ()
+	if commandsUsed[position] == true then return end
+	commandsUsed[position] = true
+	
+	if commands[position].command == "acc" then
+		acc = acc + commands[position].value
+		position = position + 1
+	elseif commands[position].command == "nop"  then
+		position = position + 1
+	elseif commands[position].command == "jmp" then
+		position = position + commands[position].value
+	end
+	
+	run()
+end
+
+run()
+
+print (acc)
