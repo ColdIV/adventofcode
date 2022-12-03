11c3382577730f9bbeae1b0eb05caa95787afc4f
Josh <dev@cldv.org>
2022-12-03 18:00:54 +0100

    update template.lua

--- template/template.lua
+++ template/template.lua
@@ -1,12 +1,12 @@
-function file_exists(file)
+local function file_exists(file)
   local f = io.open(file, "rb")
   if f then f:close() end
   return f ~= nil
 end
 
-function lines_from(file)
+local function lines_from(file)
   if not file_exists(file) then return {} end
-  lines = {}
+  local lines = {}
   for line in io.lines(file) do 
     lines[#lines + 1] = line
   end
