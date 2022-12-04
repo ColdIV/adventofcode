1adfec9a174c9e06cd8c68b02b5106cc07679650
Josh <dev@cldv.org>
2022-12-04 11:29:59 +0100

    day4-2 of aoc 2022

---
+++ 2022/04/day4-2.lua
@@ -0,0 +1,52 @@
+local function file_exists(file)
+  local f = io.open(file, "rb")
+  if f then f:close() end
+  return f ~= nil
+end
+
+local function lines_from(file)
+  if not file_exists(file) then return {} end
+  local lines = {}
+  for line in io.lines(file) do 
+    lines[#lines + 1] = line
+  end
+  return lines
+end
+
+---
+
+---@diagnostic disable-next-line: duplicate-set-field
+function string:split(sep)
+  sep = sep or ":"
+  local fields = {}
+  local pattern = string.format("([^%s]+)", sep)
+  local _ = self:gsub(pattern, function(c) fields[#fields+1] = c end)
+  return fields
+end
+
+local function containsPair(tbl)
+  if tbl[1].lower > tbl[2].higher or tbl[1].higher < tbl[2].lower or
+     tbl[2].lower > tbl[1].higher or tbl[2].higher < tbl[1].lower then
+      return 0
+  end
+
+  return 1
+end
+
+local file = 'data.txt'
+local lines = lines_from(file)
+local sum = 0
+
+for k, v in pairs(lines) do
+  local _pairs = v:split(',')
+  local obj = {}
+  for i=1,2 do
+    local tmp = _pairs[i]:split('-')
+    obj[i] = {}
+    obj[i].lower = tonumber(tmp[1])
+    obj[i].higher = tonumber(tmp[2])
+  end
+  sum = sum + containsPair(obj)
+end
+
+print (sum)
